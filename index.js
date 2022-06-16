39f28cb23867dff0a080bb676e685b7c0ea13207
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-06-16 14:33:07 -0400

    add compute functionality

--- index.js
+++ index.js
@@ -6,20 +6,53 @@ document.querySelectorAll('div.clickElement').forEach(item => {
   item.addEventListener('click', event => {
     itemClicked = item.children[0].innerHTML
     if (itemClicked === "AC"){
-      array = []
+      displayArray = []
+      updateDisplay();
     }
     else if (itemClicked === "DEL") {
       deleteLastItem();
+      updateDisplay();
     }
-    else {
+    else if (itemClicked === "="){
+      compute();
+    }
+    else{
       displayArray.push(itemClicked)
     }
-    document.querySelector("p.display-numbers").innerHTML = displayArray.join("");;
+    updateDisplay();
   })
 })
 
+function updateDisplay(){
+  document.querySelector("p.display-numbers").innerHTML = displayArray.join("");
+}
+
 function deleteLastItem (){
   displayArray.pop()
+}
+
+function compute(){
+  output = displayArray[0];
+  for (var i = 0; i < displayArray.length; i++){
+    if (displayArray[i] === "×"){
+      output = output * displayArray[i+1]
+    }
+    if (displayArray[i] === "+"){
+      output = Number(output) + Number(displayArray[i+1])
+    }
+    if (displayArray[i] === "-"){
+      output = output - displayArray[i+1]
+    }
+    if (displayArray[i] === "÷"){
+      output = output / displayArray[i+1]
+    }
+  }
+  output = Math.round(output * 100) / 100;
+  if (isNaN(output)){
+    output = 0
+  }
+  displayArray = [output]
+  updateDisplay();
 }
 
 
