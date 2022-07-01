cb83b8650d8a9f0473152dd1f8d57021fb767e88
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-07-01 01:39:35 -0400

    removed node_modules

--- index.js
+++ index.js
@@ -1,78 +1,73 @@
+//Footer Date
+const footerDate = new Date().getFullYear();
+document.querySelector("span.current-year").innerHTML = footerDate;
+
 //Display input
-displayArray = []
+displayArray = [];
 
 //Handle user input
-document.querySelectorAll('div.clickElement').forEach(item => {
-  item.addEventListener('click', event => {
-    itemClicked = item.children[0].innerHTML
-    if (itemClicked === "AC"){
-      displayArray = []
+document.querySelectorAll("div.clickElement").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    itemClicked = item.children[0].innerHTML;
+    if (itemClicked === "AC") {
+      displayArray = [];
       updateDisplay();
-    }
-    else if (itemClicked === "DEL") {
+    } else if (itemClicked === "DEL") {
       deleteLastItem();
       updateDisplay();
-    }
-    else if (itemClicked === "="){
+    } else if (itemClicked === "=") {
       preCompute();
-    }
-    else{
-      displayArray.push(itemClicked)
+    } else {
+      displayArray.push(itemClicked);
     }
     updateDisplay();
-  })
-})
+  });
+});
 
-function updateDisplay(){
+function updateDisplay() {
   document.querySelector("p.display-numbers").innerHTML = displayArray.join("");
 }
 
-function deleteLastItem (){
-  displayArray.pop()
+function deleteLastItem() {
+  displayArray.pop();
 }
 
-function preCompute(){
-  computeArray = []
-  tempValue = ""
-  for (var i = 0; i < displayArray.length; i++){
-    if (!isNaN(Number(displayArray[i])) || displayArray[i] === "."){
-      tempValue = tempValue + displayArray[i]
-    }
-    else {
-      computeArray.push(Number(tempValue))
-      tempValue = ""
-      computeArray.push(displayArray[i])
+function preCompute() {
+  computeArray = [];
+  tempValue = "";
+  for (var i = 0; i < displayArray.length; i++) {
+    if (!isNaN(Number(displayArray[i])) || displayArray[i] === ".") {
+      tempValue = tempValue + displayArray[i];
+    } else {
+      computeArray.push(Number(tempValue));
+      tempValue = "";
+      computeArray.push(displayArray[i]);
     }
   }
-  computeArray.push(Number(tempValue))
+  computeArray.push(Number(tempValue));
   compute();
 }
 
-function compute(){
+function compute() {
   output = computeArray[0];
-  for (var i = 0; i < computeArray.length; i++){
-    if (computeArray[i] === "×"){
-      output = output * computeArray[i+1]
+  for (var i = 0; i < computeArray.length; i++) {
+    if (computeArray[i] === "×") {
+      output = output * computeArray[i + 1];
     }
-    if (computeArray[i] === "+"){
-      output = Number(output) + Number(computeArray[i+1])
+    if (computeArray[i] === "+") {
+      output = Number(output) + Number(computeArray[i + 1]);
     }
-    if (computeArray[i] === "-"){
-      output = output - computeArray[i+1]
+    if (computeArray[i] === "-") {
+      output = output - computeArray[i + 1];
     }
-    if (computeArray[i] === "÷"){
-      output = output / computeArray[i+1]
+    if (computeArray[i] === "÷") {
+      output = output / computeArray[i + 1];
     }
   }
   output = Math.round(output * 100) / 100;
-  if (isNaN(output)){
-    output = 0
+  if (isNaN(output)) {
+    output = 0;
   }
-  displayArray = [output]
+  displayArray = [output];
   updateDisplay();
 }
-
-
-
-
-
