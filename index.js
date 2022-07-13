35f7a477935dc1b0300711cb1518fcacb6c3d0e8
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-07-12 21:16:14 -0400

    refactored code to pass ESLint according to Airbnb Styleguide

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
-//Footer Date
+// Footer Date
 const footerDate = new Date().getFullYear();
-document.querySelector("span.current-year").innerHTML = footerDate;
+document.querySelector('span.current-year').innerHTML = footerDate;
 
-//Display input
-displayArray = [];
+// Display input
+let displayArray = [];
 
-//Handle user input
-document.querySelectorAll("div.clickElement").forEach((item) => {
-  item.addEventListener("click", (event) => {
-    itemClicked = item.children[0].innerHTML;
-    if (itemClicked === "AC") {
+function updateDisplay() {
+  document.querySelector('p.display-numbers').innerHTML = displayArray.join('');
+}
+
+function deleteLastItem() {
+  displayArray.pop();
+}
+
+function compute(computeArray) {
+  let output = computeArray[0];
+  for (let i = 0; i < computeArray.length; i += 1) {
+    if (computeArray[i] === '×') {
+      output *= computeArray[i + 1];
+    }
+    if (computeArray[i] === '+') {
+      output = Number(output) + Number(computeArray[i + 1]);
+    }
+    if (computeArray[i] === '-') {
+      output -= computeArray[i + 1];
+    }
+    if (computeArray[i] === '÷') {
+      output /= computeArray[i + 1];
+    }
+  }
+  output = Math.round(output * 100) / 100;
+  if (Number.isNaN(output)) {
+    output = 0;
+  }
+  displayArray = [output];
+  updateDisplay();
+}
+
+function preCompute() {
+  const computeArray = [];
+  let tempValue = '';
+  for (let i = 0; i < displayArray.length; i += 1) {
+    if (!Number.isNaN(Number(displayArray[i])) || displayArray[i] === '.') {
+      tempValue += displayArray[i];
+    } else {
+      computeArray.push(Number(tempValue));
+      tempValue = '';
+      computeArray.push(displayArray[i]);
+    }
+  }
+  computeArray.push(Number(tempValue));
+  compute(computeArray);
+}
+
+// Handle user input
+document.querySelectorAll('div.clickElement').forEach((item) => {
+  item.addEventListener('click', () => {
+    const itemClicked = item.children[0].innerHTML;
+    if (itemClicked === 'AC') {
       displayArray = [];
       updateDisplay();
-    } else if (itemClicked === "DEL") {
+    } else if (itemClicked === 'DEL') {
       deleteLastItem();
       updateDisplay();
-    } else if (itemClicked === "=") {
+    } else if (itemClicked === '=') {
       preCompute();
     } else {
       displayArray.push(itemClicked);
@@ -23,51 +71,3 @@ document.querySelectorAll("div.clickElement").forEach((item) => {
     updateDisplay();
   });
 });
-
-function updateDisplay() {
-  document.querySelector("p.display-numbers").innerHTML = displayArray.join("");
-}
-
-function deleteLastItem() {
-  displayArray.pop();
-}
-
-function preCompute() {
-  computeArray = [];
-  tempValue = "";
-  for (var i = 0; i < displayArray.length; i++) {
-    if (!isNaN(Number(displayArray[i])) || displayArray[i] === ".") {
-      tempValue = tempValue + displayArray[i];
-    } else {
-      computeArray.push(Number(tempValue));
-      tempValue = "";
-      computeArray.push(displayArray[i]);
-    }
-  }
-  computeArray.push(Number(tempValue));
-  compute();
-}
-
-function compute() {
-  output = computeArray[0];
-  for (var i = 0; i < computeArray.length; i++) {
-    if (computeArray[i] === "×") {
-      output = output * computeArray[i + 1];
-    }
-    if (computeArray[i] === "+") {
-      output = Number(output) + Number(computeArray[i + 1]);
-    }
-    if (computeArray[i] === "-") {
-      output = output - computeArray[i + 1];
-    }
-    if (computeArray[i] === "÷") {
-      output = output / computeArray[i + 1];
-    }
-  }
-  output = Math.round(output * 100) / 100;
-  if (isNaN(output)) {
-    output = 0;
-  }
-  displayArray = [output];
-  updateDisplay();
-}
